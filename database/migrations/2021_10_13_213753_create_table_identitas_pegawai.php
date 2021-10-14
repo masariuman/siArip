8d7d9aba806e0b30daeab008b7c85c978554c657
masariuman <masariuman@gmail.com>
2021-10-14 10:57:13 +0700

    --- Auto-Git Commit ---

--- database/migrations/2021_10_13_213753_create_table_identitas_pegawai.php
+++ database/migrations/2021_10_13_213753_create_table_identitas_pegawai.php
@@ -20,28 +20,28 @@ class CreateTableIdentitasPegawai extends Migration
             $table->unsignedBigInteger('pegawai_id');
             $table->foreign('pegawai_id')->references('id')->on('users');
 
-            $table->text('alamat')->nullable()->default('');
-            $table->string('telepon')->nullable()->default('');
-            $table->string('handphone')->nullable()->default('');
-            $table->string('emailDinas')->nullable()->default('');
-            $table->string('emailPribadi')->nullable()->default('');
-            $table->string('nik')->nullable()->default('');
-            $table->string('nomorKK')->nullable()->default('');
+            $table->text('alamat')->default('');
+            $table->string('telepon')->default('');
+            $table->string('handphone')->default('');
+            $table->string('emailDinas')->default('');
+            $table->string('emailPribadi')->default('');
+            $table->string('nik')->default('');
+            $table->string('nomorKK')->default('');
 
             $table->unsignedBigInteger('agama_id')->nullable();
             $table->foreign('agama_id')->references('id')->on('ref_agama');
 
-            $table->string('lokasiKerja')->nullable()->default('');
-            $table->string('akta')->nullable()->default('');
-            $table->string('npwp')->nullable()->default('');
+            $table->string('lokasiKerja')->default('');
+            $table->string('akta')->default('');
+            $table->string('npwp')->default('');
             $table->date('tanggalNpwp')->nullable();
-            $table->string('bpjs')->nullable()->default('');
-            $table->string('karis')->nullable()->default('');
-            $table->string('taspen')->nullable()->default('');
+            $table->string('bpjs')->default('');
+            $table->string('karis')->default('');
+            $table->string('taspen')->default('');
             $table->date('tanggalTaspen')->nullable();
-            $table->string('tapera')->nullable()->default('');
-            $table->string('kppn')->nullable()->default('');
-            $table->string('kelasJabatan')->nullable()->default('');
+            $table->string('tapera')->default('');
+            $table->string('kppn')->default('');
+            $table->string('kelasJabatan')->default('');
             $table->timestamps();
         });
     }
